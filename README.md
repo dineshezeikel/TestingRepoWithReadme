73d04b3b0e1da5bdf42978891f72252c942a96cf
DineshEzeikel <dinesh.ezeikel17@gmail.com>
2021-09-25 11:30:20 +0530

    Add new functionalities

--- README.md
+++
@@ -1,4 +0,0 @@
-# Testing Repository With README
-
-This is a repository which will demonstrate the use of README.md file. This project does
-nothing, apart from a demo purpose.
---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+const clickBtn = document.querySelector("#btnDisplay");
+clickBtn.addEventListener("click", (e) => {
+  e.preventDefault;
+  alert("In god i trust");
+});
